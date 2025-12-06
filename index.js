dc7b3b4292b81aafdde2024343007f63577f955a
Reymon Ruiz Reyes <20240081@itla.edu.do>
2025-12-06 10:30:56 -0400

    Primer commit

---
+++ index.js
@@ -0,0 +1,16 @@
+import express from 'express';
+import { fileURLToPath } from 'url';
+import path from 'path';
+const app = express();
+const port = 3000
+
+export const __filename = fileURLToPath(import.meta.url);
+export const __dirname = path.dirname(__filename);
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'home.html'));
+})
+
+app.listen(port, () => {
+    console.log(`Aplicación ejecutandose en http://localhost:${port}`)
+})
